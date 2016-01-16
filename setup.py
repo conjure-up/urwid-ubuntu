971dcab4685caec52306e5dbb67ea4b2d6e878f4
Adam Stokes <adam.stokes@ubuntu.com>
2016-01-16 11:46:54 +0000

    bump rev
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Ubuntu styled widgets for urwid.
 from setuptools import setup, find_packages
 
 setup(name='ubuntui',
-      version="0.0.2",
+      version="0.0.3",
       description="Urwid widgets used in Ubuntu console apps",
       long_description=__doc__,
       author="Canonical Solutions Engineering",
