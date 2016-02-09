e70c84e949dc83251c0f8984e6a2a1cd1113420d
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-09 10:52:49 -0500

    no vertical bars
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- ubuntui/widgets/table.py
+++ ubuntui/widgets/table.py
@@ -19,17 +19,12 @@ class Table:
         for h in headings:
             h = Color.column_header(h)
             cols.append(h)
-            cols.append(('fixed', 1,
-                         Color.column_header(
-                             Divider("\N{BOX DRAWINGS LIGHT VERTICAL}"))))
         self.addRow(Columns(cols))
 
     def addColumns(self, columns):
         cols = []
         for c in columns:
             cols.append(c)
-            cols.append(('fixed', 1,
-                         Divider("\N{BOX DRAWINGS LIGHT VERTICAL}")))
         self.addRow(Columns(cols))
 
     def addRow(self, item):
