feba648245e50bc7f013243b31bd9aebb03249af
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-11 10:57:18 -0500

    Logic for showing a divider
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- ubuntui/widgets/table.py
+++ ubuntui/widgets/table.py
@@ -33,14 +33,26 @@ class Table:
         if row_id not in self._row_id or force:
             if row_id not in self._row_id:
                 self._row_id.append(row_id)
-            self.addRow(Columns(columns))
 
-    def addRow(self, item):
+            # If we force an additional row it's usually to expand
+            # on the previous row so we do not display a divider
+            # between the two.
+            use_divider = True
+            if force:
+                use_divider = False
+            self.addRow(Columns(columns), use_divider)
+
+    def addRow(self, item, use_divider=True):
         """ Appends widget to Pile
+
+        Arguments:
+        item: Widget to add to listbox
+        use_divider: use divider for row item
         """
+        if use_divider:
+            self._rows.append(
+                Divider("\N{BOX DRAWINGS LIGHT HORIZONTAL}"))
         self._rows.append(item)
-        self._rows.append(
-            Divider("\N{BOX DRAWINGS LIGHT HORIZONTAL}"))
 
     def render(self):
         return ListBox(self._rows)
