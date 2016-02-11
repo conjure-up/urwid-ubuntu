9675ffa2e16fced44bb8dd04536bacdc51881b60
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-11 10:57:18 -0500

    Logic for showing a divider
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- ubuntui/widgets/table.py
+++ ubuntui/widgets/table.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 
 from urwid import (Columns, ListBox, Divider)
-from ubuntui.utils import Color
 
 
 class Table:
     def __init__(self):
         self._rows = []
         self._row_id = []
+        self._is_header_set = False
 
     def addHeadings(self, headings):
         """ Takes list of headings and converts them to column header
@@ -16,11 +16,9 @@ class Table:
         Params:
         headings: List of column text headings
         """
-        cols = []
-        for h in headings:
-            h = Color.column_header(h)
-            cols.append(h)
-        self.addRow(Columns(cols))
+        if not self._is_header_set:
+            self.addRow(Columns(headings))
+            self._is_header_set = True
 
     def addColumns(self, row_id, columns, force=False):
         """ Convert list of widgets to Columns and add to a table row
@@ -33,14 +31,26 @@ class Table:
         if row_id not in self._row_id or force:
             if row_id not in self._row_id:
                 self._row_id.append(row_id)
-            self.addRow(Columns(columns))
 
-    def addRow(self, item):
+            # If we force an additional row it's usually to expand
+            # on the previous row so we do not display a divider
+            # between the two.
+            use_divider = True
+            if force:
+                use_divider = False
+            self.addRow(Columns(columns), use_divider)
+
+    def addRow(self, item, use_divider=True):
         """ Appends widget to Pile
+
+        Arguments:
+        item: Widget to add to listbox
+        use_divider: use divider for row item
         """
+        if use_divider:
+            self._rows.append(
+                Divider("\N{BOX DRAWINGS LIGHT HORIZONTAL}"))
         self._rows.append(item)
-        self._rows.append(
-            Divider("\N{BOX DRAWINGS LIGHT HORIZONTAL}"))
 
     def render(self):
         return ListBox(self._rows)
