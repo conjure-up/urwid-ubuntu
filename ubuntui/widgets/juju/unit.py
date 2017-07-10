1efe37e857abcb4a9832e5e59b7134baa429bf9e
Cory Johns <johnsca@gmail.com>
2017-07-10 14:10:18 -0400

    Fix TagMarkupExceptions in Juju widget (#8)

--- ubuntui/widgets/juju/unit.py
+++ ubuntui/widgets/juju/unit.py
@@ -15,16 +15,16 @@ class UnitWidget:
         self._unit = unit
         self._name = name
         self.Name = Text(self._name)
-        self.PublicAddress = Text(unit.get('public-address', ''))
-        self.Machine = Text(unit.get('machine', ''))
+        self.PublicAddress = Text(unit.get('public-address') or '')
+        self.Machine = Text(unit.get('machine') or '')
 
         agent_status = unit.get('agent-status', {})
         if agent_status:
-            self.AgentStatus = Text(agent_status.get('status', ''))
-            self.AgentStatusInfo = Text(agent_status.get('info', ''))
+            self.AgentStatus = Text(agent_status.get('status') or '')
+            self.AgentStatusInfo = Text(agent_status.get('info') or '')
 
         workload = unit.get('workload-status', {})
         if workload:
-            self.WorkloadInfo = Text(workload.get('info', ''))
-            self.WorkloadStatus = Text(workload.get('status', ''))
+            self.WorkloadInfo = Text(workload.get('info') or '')
+            self.WorkloadStatus = Text(workload.get('status') or '')
         self.Icon = Text("")
